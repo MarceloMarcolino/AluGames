aec0fd313845e74a6d9786f470081b30e5f1176e
Marcelo Antonio Pereira Marcolino <marcelo.marcolino.dev@gmail.com>
2025-04-01 23:45:09 -0300

    Confirmação de ações & registro no console

--- js/app.js
+++ js/app.js
@@ -1,18 +1,49 @@
+let jogosAlugados = 0;
+let jogosDisponiveis = 0;
+
+function contarEExibirJogosAlugados() {
+    console.log(`Total de jogos alugados: ${jogosAlugados}`);
+}
+
+function contarEExibirJogosDisponíveis() {
+    console.log(`Total de jogos disponíveis: ${jogosDisponiveis}`);
+}
+
 function alterarStatus(id){
     let gameClicado = document.getElementById(`game-${id}`);
     let imagem = gameClicado.querySelector('.dashboard__item__img');
     let botao = gameClicado.querySelector('.dashboard__item__button');
-
-    //let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
+    let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
     //alert(nomeJogo.textContent);
 
     if (imagem.classList.contains('dashboard__item__img--rented')) {
-        imagem.classList.remove('dashboard__item__img--rented');
-        botao.classList.remove('dashboard__item__button--return');
-        botao.textContent = 'Alugar';
+        // Adiciona uma confirmação antes de devolver o jogo
+        if (confirm(`Você tem certeza que deseja devolver o jogo ${nomeJogo.textContent}?`)){
+            imagem.classList.remove('dashboard__item__img--rented');
+            botao.classList.remove('dashboard__item__button--return');
+            botao.textContent = 'Alugar';
+            jogosAlugados--;
+            jogosDisponiveis++;
+        }
     } else {
-        imagem.classList.add('dashboard__item__img--rented');
-        botao.classList.add('dashboard__item__button--return');
-        botao.textContent = 'Devolver';
+        // Adiciona uma confirmação antes de alugar o jogo
+        if (confirm(`Você tem certeza que deseja alugar o jogo ${nomeJogo.textContent}?`)){
+            imagem.classList.add('dashboard__item__img--rented');
+            botao.classList.add('dashboard__item__button--return');
+            botao.textContent = 'Devolver';
+            jogosAlugados++;
+            jogosDisponiveis--;
+        }
     }
+
+    contarEExibirJogosAlugados();
+    contarEExibirJogosDisponíveis();
 }
+
+// Inicializa a contagem considerando que os jogos já começam alugados
+document.addEventListener('DOMContentLoaded', function() {
+    jogosAlugados = document.querySelectorAll('.dashboard__item__img--rented').length;
+    jogosDisponiveis = document.querySelectorAll('.dashboard__item__img').length - jogosAlugados;
+    contarEExibirJogosAlugados();
+    contarEExibirJogosDisponíveis();
+})
